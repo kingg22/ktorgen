1a11104d426e4cd9e9765eb01ee36fef5b08f652
Rey <89825425+Kingg22@users.noreply.github.com>
2025-07-30 12:09:43 -0500

    build(deps): add new plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,12 @@ plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
     alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.androidMultiplatformLibrary) apply false
     alias(libs.plugins.dokka) apply false
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlinxKover) apply false
+    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.ktlint) apply false
-    alias(libs.plugins.maven.publish) apply false
+    alias(libs.plugins.mavenPublish) apply false
 }
