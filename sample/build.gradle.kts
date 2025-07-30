f0b0c5d7662974650d1e175d9b5e4205920af22c
Rey <89825425+Kingg22@users.noreply.github.com>
2025-07-30 12:26:12 -0500

    build(ktorgen-sample): add a sample module with all targets of annotations
    TODO add android app or compose multiplatform app in samples

---
+++ sample/build.gradle.kts
@@ -0,0 +1,112 @@
+@file:Suppress("UnstableApiUsage")
+@file:OptIn(ExperimentalWasmDsl::class)
+
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.ktlint)
+}
+
+group = "io.github.kingg22"
+version = libs.versions.ktorgen.version.get()
+
+kotlin {
+    compilerOptions {
+        extraWarnings.set(true)
+        allWarningsAsErrors.set(true)
+    }
+
+    applyDefaultHierarchyTemplate()
+
+    // TODO add a android app to samples
+    androidTarget {
+        publishLibraryVariants("release")
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_1_8)
+        }
+    }
+
+    jvm {
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_1_8)
+        }
+    }
+
+    js {
+        nodejs()
+    }
+
+    wasmWasi {
+        nodejs()
+    }
+
+    // Tiers are in accordance with <https://kotlinlang.org/docs/native-target-support.html>
+    // Tier 1
+    macosX64()
+    macosArm64()
+    iosSimulatorArm64()
+    iosX64()
+    iosArm64()
+    // Tier 2
+    linuxX64()
+    linuxArm64()
+    watchosSimulatorArm64()
+    watchosX64()
+    watchosArm32()
+    watchosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    tvosArm64()
+    // Tier 3
+    androidNativeArm32()
+    androidNativeArm64()
+    androidNativeX86()
+    androidNativeX64()
+    mingwX64()
+    watchosDeviceArm64()
+
+    sourceSets.commonMain.dependencies {
+        implementation(projects.annotations)
+    }
+}
+
+dependencies {
+    kspCommonMainMetadata(projects.compiler)
+}
+
+android {
+    namespace = "$group.ktorgen.sample"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+    testOptions {
+        unitTests.all {
+            it.useJUnitPlatform()
+        }
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+ktlint {
+    version.set(libs.versions.ktlint.pinterest.get())
+}
+
+ksp {
+    arg("ktorgen_check_type", "2")
+}
