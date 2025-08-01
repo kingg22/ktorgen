1e7ed4f3941e50184b4b8bba18aba89d022f9374
Rey <89825425+Kingg22@users.noreply.github.com>
2025-08-01 18:43:48 -0500

    perf(ktorgen-compiler): move initial imports if functions aren't empty

--- compiler/src/main/kotlin/io/github/kingg22/ktorgen/extractor/ClassMapper.kt
+++ compiler/src/main/kotlin/io/github/kingg22/ktorgen/extractor/ClassMapper.kt
@@ -16,39 +16,45 @@ import io.github.kingg22.ktorgen.model.KTOR_URL_TAKE_FROM
 
 class ClassMapper : DeclarationMapper {
     override fun mapToModel(declaration: KSClassDeclaration): ClassData {
-        val imports = mutableSetOf(
-            KTOR_CLIENT_HTTP_REQUEST_BUILDER,
-            KTOR_CLIENT_PARAMETER,
-            KTOR_URL_BUILDER,
-            KTOR_URL_TAKE_FROM,
-            KTOR_DECODE_URL_QUERY,
-        )
+        val imports = mutableSetOf<String>()
 
         val packageName = declaration.packageName.asString()
         val className = declaration.simpleName.asString()
 
         val functions = declaration.getDeclaredFunctions().map { func ->
             DeclarationFunctionMapper.DEFAULT.mapToModel(func, imports::add)
-        }.toList()
+        }
 
-        val filteredSupertypes =
-            declaration.superTypes.toList().filterNot {
-                /** In KSP Any is a supertype of an interface */
-                it.toTypeName() == ANY
-            }
+        val filteredSupertypes = declaration.superTypes.filterNot {
+            /** In KSP Any is a supertype of an interface */
+            it.toTypeName() == ANY
+        }
 
-        val properties = declaration.getDeclaredProperties().toList()
+        val properties = declaration.getDeclaredProperties()
 
+        // an operation terminal of sequences must be in one site
         return ClassData(
             interfaceName = className,
             packageName = packageName,
-            functions = functions,
+            functions = functions.toList().also {
+                if (it.isNotEmpty()) {
+                    imports.addAll(
+                        arrayOf(
+                            KTOR_CLIENT_HTTP_REQUEST_BUILDER,
+                            KTOR_CLIENT_PARAMETER,
+                            KTOR_URL_BUILDER,
+                            KTOR_URL_TAKE_FROM,
+                            KTOR_DECODE_URL_QUERY,
+                        ),
+                    )
+                }
+            },
             imports = imports,
-            superClasses = filteredSupertypes,
-            properties = properties,
+            superClasses = filteredSupertypes.toList(),
+            properties = properties.toList(),
             modifiers = declaration.modifiers.mapNotNull { it.toKModifier() },
             ksFile = requireNotNull(declaration.containingFile) {
-                "${KtorGenLogger.KTOR_GEN} Interface $className must be in a file, was null"
+                "${KtorGenLogger.KTOR_GEN} Interface $className must be in a file, but was null"
             },
             annotations = declaration.annotations.toSet(),
         )
