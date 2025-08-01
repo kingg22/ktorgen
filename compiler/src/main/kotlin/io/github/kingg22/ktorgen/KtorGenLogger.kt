191c4e0d3b9eae16d4e19ab68ef99bb41c79ed60
Rey <89825425+Kingg22@users.noreply.github.com>
2025-07-31 23:39:55 -0500

    feat(ktorgen-compiler): add messages logs

--- compiler/src/main/kotlin/io/github/kingg22/ktorgen/KtorGenLogger.kt
+++ compiler/src/main/kotlin/io/github/kingg22/ktorgen/KtorGenLogger.kt
@@ -4,6 +4,10 @@ import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.symbol.KSNode
 
 class KtorGenLogger(private val kspLogger: KSPLogger, private val loggingType: Int) : KSPLogger by kspLogger {
+    init {
+        instance = this
+    }
+
     override fun error(message: String, symbol: KSNode?) {
         when (loggingType) {
             0 -> {
@@ -11,17 +15,22 @@ class KtorGenLogger(private val kspLogger: KSPLogger, private val loggingType: I
             }
 
             1 -> {
-                kspLogger.error("[KtorGen]: $message", symbol)
+                kspLogger.error("$KTOR_GEN $message", symbol)
             }
 
             2 -> {
                 // Turn errors into compile warnings
-                kspLogger.warn("[KtorGen]: $message", symbol)
+                kspLogger.warn("$KTOR_GEN $message", symbol)
             }
         }
     }
 
     companion object {
-        const val KTOR_GEN_TYPE_NOT_ALLOWED = "Only interfaces and it companion objects can be annotated with @KtorGen"
+        lateinit var instance: KtorGenLogger
+        const val KTOR_GEN = "[KtorGen]:"
+        const val KTOR_GEN_TYPE_NOT_ALLOWED =
+            "$KTOR_GEN Only interfaces and it companion objects can be annotated with @KtorGen"
+        const val NO_HTTP_ANNOTATION_AT = "$KTOR_GEN No Http annotation at "
+        const val ONLY_ONE_HTTP_METHOD_IS_ALLOWED = "$KTOR_GEN Only one HTTP method is allowed."
     }
 }
