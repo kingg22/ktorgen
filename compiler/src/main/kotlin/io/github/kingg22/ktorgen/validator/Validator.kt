ef8110e992d58f5a5afd3473d7004d7846d33185
Rey <89825425+Kingg22@users.noreply.github.com>
2025-07-31 23:39:35 -0500

    perf(ktorgen-compiler): default of interface by lazy

--- compiler/src/main/kotlin/io/github/kingg22/ktorgen/validator/Validator.kt
+++ compiler/src/main/kotlin/io/github/kingg22/ktorgen/validator/Validator.kt
@@ -8,7 +8,9 @@ fun interface Validator {
     fun validate(classData: ClassData, ktorGenOptions: KtorGenOptions, ktorGenLogger: KtorGenLogger): ClassData?
 
     companion object {
-        val DEFAULT = Validator { data, _, _ -> data }
+        val DEFAULT by lazy {
+            Validator { data, _, _ -> data }
+        }
 
         fun validate(classData: ClassData, ktorGenOptions: KtorGenOptions, ktorGenLogger: KtorGenLogger) =
             DEFAULT.validate(classData, ktorGenOptions, ktorGenLogger)
