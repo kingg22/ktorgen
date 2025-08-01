24be7d1cd2d77855a339674447235579dd93e452
Rey <89825425+Kingg22@users.noreply.github.com>
2025-07-31 23:40:22 -0500

    test(ktorgen-sample): mini test raw mapper, success

--- sample/src/commonMain/kotlin/DemoGithub.kt
+++ sample/src/commonMain/kotlin/DemoGithub.kt
@@ -1,6 +1,16 @@
 package io.github.kingg22.ktorgen.sample
 
 import io.github.kingg22.ktorgen.core.KtorGen
+import io.github.kingg22.ktorgen.http.GET
+import io.github.kingg22.ktorgen.http.Headers
+import io.github.kingg22.ktorgen.http.Path
 
 @KtorGen
-interface DemoGithub
+interface DemoGithub {
+    @GET("repos/{owner}/{repo}")
+    suspend fun getRepo(@Path owner: String, @Path repo: String): String
+
+    @Headers("Content-Type: application/json")
+    @GET("users")
+    suspend fun getUsers(): List<String>
+}
