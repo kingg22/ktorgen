f49851edd69e554113225cdf1817a59b1ea2e2cd
Rey <89825425+Kingg22@users.noreply.github.com>
2025-08-01 20:33:06 -0500

    feat(ktorgen-compiler): add messages logs

--- compiler/src/main/kotlin/io/github/kingg22/ktorgen/KtorGenLogger.kt
+++ compiler/src/main/kotlin/io/github/kingg22/ktorgen/KtorGenLogger.kt
@@ -32,5 +32,13 @@ class KtorGenLogger(private val kspLogger: KSPLogger, private val loggingType: I
             "$KTOR_GEN Only interfaces and it companion objects can be annotated with @KtorGen"
         const val NO_HTTP_ANNOTATION_AT = "$KTOR_GEN No Http annotation at "
         const val ONLY_ONE_HTTP_METHOD_IS_ALLOWED = "$KTOR_GEN Only one HTTP method is allowed."
+        const val FUNCTION_OR_PARAMETERS_TYPES_MUST_NOT_INCLUDE_TYPE_VARIABLE_OR_WILDCARD =
+            "$KTOR_GEN Function or parameters types must not include a type variable or wildcard:"
+        const val FORM_URL_ENCODED_CAN_ONLY_BE_SPECIFIED_ON_HTTP_METHODS_WITH_REQUEST_BODY =
+            "$KTOR_GEN FormUrlEncoded can only be specified on HTTP methods with request body (e.g., @POST)."
+        const val MULTIPART_CAN_ONLY_BE_SPECIFIED_ON_HTTP_METHODS =
+            "$KTOR_GEN Multipart can only be specified on HTTP methods with request body (e.g., @POST)"
+        const val GET_METHOD_MUST_NOT_INCLUDE_BODY =
+            " method must not include body. See https://datatracker.ietf.org/doc/html/rfc7231#section-4.3.1"
     }
 }
