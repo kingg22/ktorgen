f35e576df07392e8272e1ed499e685de5b809315
Rey <89825425+Kingg22@users.noreply.github.com>
2025-08-02 17:34:25 -0500

    feat(ktorgen-compiler): add own detection of data on functions

--- compiler/src/main/kotlin/io/github/kingg22/ktorgen/model/FunctionData.kt
+++ compiler/src/main/kotlin/io/github/kingg22/ktorgen/model/FunctionData.kt
@@ -1,8 +1,10 @@
 package io.github.kingg22.ktorgen.model
 
+import com.google.devtools.ksp.symbol.KSFunctionDeclaration
 import com.squareup.kotlinpoet.AnnotationSpec
 import com.squareup.kotlinpoet.KModifier
 import io.github.kingg22.ktorgen.model.annotations.FunctionAnnotation
+import io.github.kingg22.ktorgen.model.annotations.ParameterAnnotation
 
 class FunctionData(
     val name: String,
@@ -14,7 +16,8 @@ class FunctionData(
     val ktorGenAnnotations: List<FunctionAnnotation>,
     val nonKtorGenAnnotations: List<AnnotationSpec>,
     val isImplemented: Boolean = false,
-    goingToGenerate: Boolean = true,
+    val ksFunctionDeclaration: KSFunctionDeclaration,
+    goingToGenerate: Boolean = ktorGenAnnotations.contains(FunctionAnnotation.Ignore).not(),
     visibilityModifier: String = "public",
     propagateAnnotations: Boolean = true,
     annotationsToPropagate: Set<AnnotationSpec> = emptySet(),
@@ -27,4 +30,40 @@ class FunctionData(
     annotationsToPropagate = annotationsToPropagate,
     optIns = optIns,
     customHeader = customHeader,
-)
+) {
+    val urlTemplate by lazy { parseUrlTemplate(httpMethodAnnotation.path) }
+    val isBody by lazy { parameterDataList.any { it.hasAnnotation<ParameterAnnotation.Body>() } }
+    val isFormUrl by lazy {
+        hasAnnotation<FunctionAnnotation.FormUrlEncoded>() ||
+            parameterDataList.any { param ->
+                param.hasAnnotation<ParameterAnnotation.Field>() || param.hasAnnotation<ParameterAnnotation.FieldMap>()
+            }
+    }
+    val isMultipart by lazy {
+        hasAnnotation<FunctionAnnotation.Multipart>() ||
+            parameterDataList.any { param ->
+                param.hasAnnotation<ParameterAnnotation.Part>() || param.hasAnnotation<ParameterAnnotation.PartMap>()
+            }
+    }
+    val isBodyInferred by lazy { listOf(isBody, isFormUrl, isMultipart).count { it } == 1 }
+
+    inline fun <reified T : FunctionAnnotation> findAnnotationOrNull(): T? =
+        this.ktorGenAnnotations.filterIsInstance<T>().firstOrNull()
+
+    inline fun <reified T : FunctionAnnotation> hasAnnotation() = this.findAnnotationOrNull<T>() != null
+
+    data class UrlTemplateResult(val template: String, val keys: List<String>)
+
+    private fun parseUrlTemplate(url: String): UrlTemplateResult {
+        val regex = "\\{([^}]+)}".toRegex()
+        val keys = mutableListOf<String>()
+
+        val template = regex.replace(url) { match ->
+            val key = match.groupValues[1]
+            keys.add(key)
+            "%s"
+        }
+
+        return UrlTemplateResult(template, keys)
+    }
+}
