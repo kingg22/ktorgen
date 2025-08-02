c2deb4679a30c20e4788d13a256f9f4343527b2e
Rey <89825425+Kingg22@users.noreply.github.com>
2025-08-01 20:32:54 -0500

    feat(ktorgen-compiler): add validator pipeline to Validator

--- compiler/src/main/kotlin/io/github/kingg22/ktorgen/validator/Validator.kt
+++ compiler/src/main/kotlin/io/github/kingg22/ktorgen/validator/Validator.kt
@@ -3,15 +3,19 @@ package io.github.kingg22.ktorgen.validator
 import io.github.kingg22.ktorgen.KtorGenLogger
 import io.github.kingg22.ktorgen.KtorGenOptions
 import io.github.kingg22.ktorgen.model.ClassData
+import io.github.kingg22.ktorgen.validator.validators.*
 
 fun interface Validator {
     fun validate(classData: ClassData, ktorGenOptions: KtorGenOptions, ktorGenLogger: KtorGenLogger): ClassData?
 
     companion object {
-        val DEFAULT by lazy {
-            Validator { data, _, _ -> data }
-        }
+        val DEFAULT by lazy { ValidatorPipeline(listOf(GetMethodNoBodyValidator())) }
 
+        val NO_OP by lazy { Validator { _, _, _ -> null } }
+
+        val NO_VALIDATION by lazy { Validator { classData, _, _ -> classData } }
+
+        /** Shortcut to get the default validator impl */
         fun validate(classData: ClassData, ktorGenOptions: KtorGenOptions, ktorGenLogger: KtorGenLogger) =
             DEFAULT.validate(classData, ktorGenOptions, ktorGenLogger)
     }
