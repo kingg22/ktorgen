6df677bcd46c83c12cd74db5a5d2fe2b605d1fc2
Rey <89825425+Kingg22@users.noreply.github.com>
2025-08-02 20:00:43 -0500

    feat(ktorgen-compiler): add shared class when is a map or pair annotation

--- compiler/src/main/kotlin/io/github/kingg22/ktorgen/model/annotations/ParameterAnnotation.kt
+++ compiler/src/main/kotlin/io/github/kingg22/ktorgen/model/annotations/ParameterAnnotation.kt
@@ -2,17 +2,18 @@ package io.github.kingg22.ktorgen.model.annotations
 
 /** Annotation at a parameter */
 sealed class ParameterAnnotation {
+    sealed class WithMapOrPairType : ParameterAnnotation()
     data object Body : ParameterAnnotation()
     data class Path(val value: String, val encoded: Boolean) : ParameterAnnotation()
     data class Query(val value: String, val encoded: Boolean) : ParameterAnnotation()
     data class QueryName(val encoded: Boolean) : ParameterAnnotation()
-    data class QueryMap(val encoded: Boolean) : ParameterAnnotation()
+    data class QueryMap(val encoded: Boolean) : WithMapOrPairType()
     data class Header(val value: String) : ParameterAnnotation()
-    data object HeaderMap : ParameterAnnotation()
+    data object HeaderMap : WithMapOrPairType()
     data object Url : ParameterAnnotation()
     data class Field(val value: String, val encoded: Boolean) : ParameterAnnotation()
-    data class FieldMap(val encoded: Boolean) : ParameterAnnotation()
+    data class FieldMap(val encoded: Boolean) : WithMapOrPairType()
     data class Part(val value: String, val encoding: String) : ParameterAnnotation()
-    data class PartMap(val encoding: String) : ParameterAnnotation()
+    data class PartMap(val encoding: String) : WithMapOrPairType()
     data class Tag(val value: String) : ParameterAnnotation()
 }
