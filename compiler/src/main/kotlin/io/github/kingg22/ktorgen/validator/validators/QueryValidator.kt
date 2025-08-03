b82aa57937bd76ee10a23a605a6cc66d2e578a22
Rey <89825425+Kingg22@users.noreply.github.com>
2025-08-02 19:56:05 -0500

    feat(ktorgen-compiler): validate query map

---
+++ compiler/src/main/kotlin/io/github/kingg22/ktorgen/validator/validators/QueryValidator.kt
@@ -0,0 +1,27 @@
+package io.github.kingg22.ktorgen.validator.validators
+
+import io.github.kingg22.ktorgen.KtorGenLogger
+import io.github.kingg22.ktorgen.model.annotations.ParameterAnnotation
+import io.github.kingg22.ktorgen.validator.ValidationContext
+import io.github.kingg22.ktorgen.validator.ValidationResult
+import io.github.kingg22.ktorgen.validator.ValidatorStrategy
+
+class QueryValidator : ValidatorStrategy {
+    override fun validate(context: ValidationContext) = ValidationResult {
+        for (function in context.functions) {
+            function.parameterDataList.forEach { parameter ->
+                parameter.findAnnotationOrNull<ParameterAnnotation.QueryMap>()?.let {
+                    validateMapParameter(
+                        parameter,
+                        context,
+                        function,
+                        KtorGenLogger.QUERY_MAP_PARAMETER_TYPE_MUST_BE_MAP_PAIR_STRING,
+                    ) { keys, values ->
+                        // <String, String>
+                        keys != Pair("kotlin.String", false) || values != Pair("kotlin.String", false)
+                    }
+                }
+            }
+        }
+    }
+}
