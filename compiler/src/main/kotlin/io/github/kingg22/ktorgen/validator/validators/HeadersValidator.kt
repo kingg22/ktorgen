ae24a6e5d8f935613648502656aec628525034bb
Rey <89825425+Kingg22@users.noreply.github.com>
2025-08-04 15:37:34 -0500

    perf(ktorgen-compiler): prevent vararg of content type header param

--- compiler/src/main/kotlin/io/github/kingg22/ktorgen/validator/validators/HeadersValidator.kt
+++ compiler/src/main/kotlin/io/github/kingg22/ktorgen/validator/validators/HeadersValidator.kt
@@ -7,20 +7,22 @@ import io.github.kingg22.ktorgen.model.annotations.ParameterAnnotation
 import io.github.kingg22.ktorgen.validator.ValidationContext
 import io.github.kingg22.ktorgen.validator.ValidationResult
 import io.github.kingg22.ktorgen.validator.ValidatorStrategy
+import io.github.kingg22.ktorgen.http.Header.Companion.ContentType as CONTENT_TYPE_HEADER
 
 class HeadersValidator : ValidatorStrategy {
     override fun validate(context: ValidationContext) = ValidationResult {
         for (function in context.functions) {
             var haveHeadersMap = function.parameterDataList.any { it.hasAnnotation<ParameterAnnotation.HeaderMap>() }
-            val headersFunction = function.findAnnotationOrNull<FunctionAnnotation.Headers>()?.value?.map {
-                it.replace("\\s+".toRegex(), "")
-            }?.toSet()?.associate {
-                val (name, value) = it.split(":", limit = 2)
-                if (name.isBlank() || value.isBlank()) {
-                    addError(KtorGenLogger.INVALID_HEADER_FORMAT + addDeclaration(context, function))
-                }
-                Pair(name.trim(), value.trim())
-            }?.toMutableMap() ?: mutableMapOf()
+            val headersFunction =
+                function.findAnnotationOrNull<FunctionAnnotation.Headers>()?.value?.map { (name, value) ->
+                    name.trim().replace("\\s+".toRegex(), "") to value.trim().replace("\\s+".toRegex(), "")
+                }?.toSet()?.associate {
+                    val (name, value) = it
+                    if (name.isBlank() || value.isBlank()) {
+                        addError(KtorGenLogger.INVALID_HEADER + addDeclaration(context, function))
+                    }
+                    it
+                }?.toMutableMap() ?: mutableMapOf()
             var haveContentType = headersFunction.any { (key, _) -> key.equals(CONTENT_TYPE_HEADER, true) }
 
             if (haveContentType && (function.isFormUrl || function.isMultipart)) {
@@ -28,19 +30,21 @@ class HeadersValidator : ValidatorStrategy {
             }
 
             for (parameter in function.parameterDataList) {
-                // Validate @Header
+                // Validate @HeaderParam
                 parameter.findAnnotationOrNull<ParameterAnnotation.Header>()?.let { header ->
                     val name = header.value.trim().replace("\\s+".toRegex(), "")
                     if (name.isBlank()) {
                         addError(
-                            KtorGenLogger.INVALID_HEADER_FORMAT + addDeclaration(context, function, parameter),
+                            KtorGenLogger.INVALID_HEADER + addDeclaration(context, function, parameter),
                         )
                     }
 
                     val isContentType = name.equals(CONTENT_TYPE_HEADER, ignoreCase = true)
 
                     // Solo valida duplicado si es Content-Type
-                    if (isContentType && headersFunction.keys.any { it.equals(CONTENT_TYPE_HEADER, true) }) {
+                    if (isContentType &&
+                        (headersFunction.keys.any { it.equals(CONTENT_TYPE_HEADER, true) } || parameter.isVararg)
+                    ) {
                         addError(KtorGenLogger.DUPLICATE_HEADER + addDeclaration(context, function, parameter))
                     }
 
@@ -77,9 +81,5 @@ class HeadersValidator : ValidatorStrategy {
                 addWarning(KtorGenLogger.CONTENT_TYPE_BODY_UNKNOWN + addDeclaration(context, function))
             }
         }
-    }
-
-    companion object {
-        const val CONTENT_TYPE_HEADER = "Content-Type"
     }
 }
