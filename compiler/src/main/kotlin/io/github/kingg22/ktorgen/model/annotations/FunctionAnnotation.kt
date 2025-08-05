1a3bffa5531f700c4f396d372c380745dbda5d53
Rey <89825425+Kingg22@users.noreply.github.com>
2025-08-04 21:35:46 -0500

    feat(ktorgen-compiler): now are interfaces mapped annotations and add cookies

--- compiler/src/main/kotlin/io/github/kingg22/ktorgen/model/annotations/FunctionAnnotation.kt
+++ compiler/src/main/kotlin/io/github/kingg22/ktorgen/model/annotations/FunctionAnnotation.kt
@@ -1,15 +1,41 @@
 package io.github.kingg22.ktorgen.model.annotations
 
 /** Annotation at a function */
-sealed class FunctionAnnotation {
-    data object Ignore : FunctionAnnotation()
-    data class HttpMethodAnnotation(val path: String, val httpMethod: HttpMethod) : FunctionAnnotation()
+sealed interface FunctionAnnotation {
+    val isRepeatable: Boolean get() = false
 
-    data class Headers(val value: Set<Pair<String, String>>) : FunctionAnnotation()
+    /** See [io.github.kingg22.ktorgen.core.KtorGenIgnore] */
+    data object Ignore : FunctionAnnotation
 
-    data object FormUrlEncoded : FunctionAnnotation()
+    /**
+     * See
+     * [io.github.kingg22.ktorgen.http.HTTP],
+     * [io.github.kingg22.ktorgen.http.GET],
+     * [io.github.kingg22.ktorgen.http.POST],
+     * [io.github.kingg22.ktorgen.http.PUT],
+     * [io.github.kingg22.ktorgen.http.DELETE],
+     * [io.github.kingg22.ktorgen.http.HEAD],
+     * [io.github.kingg22.ktorgen.http.OPTIONS],
+     * [io.github.kingg22.ktorgen.http.PATCH],
+     */
+    data class HttpMethodAnnotation(val path: String, val httpMethod: HttpMethod) : FunctionAnnotation
 
-    data object Streaming : FunctionAnnotation()
+    /** See [io.github.kingg22.ktorgen.http.Header] */
+    data class Headers(val value: List<Pair<String, String>>) : FunctionAnnotation {
+        override val isRepeatable = true
+    }
 
-    data object Multipart : FunctionAnnotation()
+    /** See [io.github.kingg22.ktorgen.http.Cookie] */
+    data class Cookies(val value: List<CookieValues>) : FunctionAnnotation {
+        override val isRepeatable = true
+    }
+
+    /** See [io.github.kingg22.ktorgen.http.FormUrlEncoded] */
+    data object FormUrlEncoded : FunctionAnnotation
+
+    /** See [io.github.kingg22.ktorgen.http.Streaming] */
+    data object Streaming : FunctionAnnotation
+
+    /** See [io.github.kingg22.ktorgen.http.Body] */
+    data object Multipart : FunctionAnnotation
 }
--- compiler/src/main/kotlin/io/github/kingg22/ktorgen/model/annotations/ParameterAnnotation.kt
+++ compiler/src/main/kotlin/io/github/kingg22/ktorgen/model/annotations/ParameterAnnotation.kt
@@ -1,19 +1,59 @@
 package io.github.kingg22.ktorgen.model.annotations
 
 /** Annotation at a parameter */
-sealed class ParameterAnnotation {
-    sealed class WithMapOrPairType : ParameterAnnotation()
-    data object Body : ParameterAnnotation()
-    data class Path(val value: String, val encoded: Boolean) : ParameterAnnotation()
-    data class Query(val value: String, val encoded: Boolean) : ParameterAnnotation()
-    data class QueryName(val encoded: Boolean) : ParameterAnnotation()
-    data class QueryMap(val encoded: Boolean) : WithMapOrPairType()
-    data class Header(val value: String) : ParameterAnnotation()
-    data object HeaderMap : WithMapOrPairType()
-    data object Url : ParameterAnnotation()
-    data class Field(val value: String, val encoded: Boolean) : ParameterAnnotation()
-    data class FieldMap(val encoded: Boolean) : WithMapOrPairType()
-    data class Part(val value: String, val encoding: String) : ParameterAnnotation()
-    data class PartMap(val encoding: String) : WithMapOrPairType()
-    data class Tag(val value: String) : ParameterAnnotation()
+sealed interface ParameterAnnotation {
+    val isRepeatable: Boolean get() = false
+    sealed interface WithMapOrPairType : ParameterAnnotation
+
+    // -- url --
+
+    /** See [io.github.kingg22.ktorgen.http.Url] */
+    data object Url : ParameterAnnotation
+
+    /** See [io.github.kingg22.ktorgen.http.Path] */
+    data class Path(val value: String, val encoded: Boolean) : ParameterAnnotation
+
+    /** See [io.github.kingg22.ktorgen.http.Query] */
+    data class Query(val value: String, val encoded: Boolean) : ParameterAnnotation
+
+    /** See [io.github.kingg22.ktorgen.http.QueryName] */
+    data class QueryName(val encoded: Boolean) : ParameterAnnotation
+
+    /** See [io.github.kingg22.ktorgen.http.QueryMap] */
+    data class QueryMap(val encoded: Boolean) : WithMapOrPairType
+
+    // -- header --
+
+    /** See [io.github.kingg22.ktorgen.http.HeaderParam] */
+    data class Header(val value: String) : ParameterAnnotation {
+        override val isRepeatable = true
+    }
+
+    /** [io.github.kingg22.ktorgen.http.HeaderMap] */
+    data object HeaderMap : WithMapOrPairType
+
+    /** See [io.github.kingg22.ktorgen.http.Cookie] */
+    data class Cookies(val value: List<CookieValues>) : ParameterAnnotation {
+        override val isRepeatable = true
+    }
+
+    // -- body --
+
+    /** See [io.github.kingg22.ktorgen.http.Body] */
+    data object Body : ParameterAnnotation
+
+    /** See [io.github.kingg22.ktorgen.http.Field] */
+    data class Field(val value: String, val encoded: Boolean) : ParameterAnnotation
+
+    /** See [io.github.kingg22.ktorgen.http.FieldMap] */
+    data class FieldMap(val encoded: Boolean) : WithMapOrPairType
+
+    /** See [io.github.kingg22.ktorgen.http.Part] */
+    data class Part(val value: String, val encoding: String) : ParameterAnnotation
+
+    /** See [io.github.kingg22.ktorgen.http.PartMap] */
+    data class PartMap(val encoding: String) : WithMapOrPairType
+
+    /** See [io.github.kingg22.ktorgen.http.Tag] */
+    data class Tag(val value: String) : ParameterAnnotation
 }
