c8392c0103d389f5f74d24f252aca9f216d99990
Rey <89825425+Kingg22@users.noreply.github.com>
2025-08-04 21:40:22 -0500

    feat(ktorgen-compiler): add warning for vararg and more than one cookie

---
+++ compiler/src/main/kotlin/io/github/kingg22/ktorgen/validator/validators/CookieValidator.kt
@@ -0,0 +1,24 @@
+package io.github.kingg22.ktorgen.validator.validators
+
+import io.github.kingg22.ktorgen.KtorGenLogger
+import io.github.kingg22.ktorgen.model.annotations.ParameterAnnotation
+import io.github.kingg22.ktorgen.validator.ValidationContext
+import io.github.kingg22.ktorgen.validator.ValidationResult
+import io.github.kingg22.ktorgen.validator.ValidatorStrategy
+
+class CookieValidator : ValidatorStrategy {
+    override fun validate(context: ValidationContext) = ValidationResult {
+        for (function in context.functions) {
+            for (parameter in function.parameterDataList.filter { it.hasAnnotation<ParameterAnnotation.Cookies>() }) {
+                if (parameter.isVararg &&
+                    parameter.ktorgenAnnotations.count { it is ParameterAnnotation.Cookies } > 1
+                ) {
+                    addWarning(
+                        KtorGenLogger.VARARG_PARAMETER_WITH_LOT_ANNOTATIONS +
+                            addDeclaration(context, function, parameter),
+                    )
+                }
+            }
+        }
+    }
+}
