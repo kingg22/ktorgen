f39ef459613f9639f4b741dfae650d9f3cb4d362
Rey <89825425+Kingg22@users.noreply.github.com>
2025-08-06 08:02:58 -0500

    feat(ktorgen-compiler): improvements and extract fragment annotation

--- compiler/src/main/kotlin/io/github/kingg22/ktorgen/extractor/FunctionMapper.kt
+++ compiler/src/main/kotlin/io/github/kingg22/ktorgen/extractor/FunctionMapper.kt
@@ -10,9 +10,9 @@ import com.squareup.kotlinpoet.KModifier
 import io.github.kingg22.ktorgen.KtorGenLogger
 import io.github.kingg22.ktorgen.Timer
 import io.github.kingg22.ktorgen.core.KtorGenExperimental
-import io.github.kingg22.ktorgen.core.KtorGenIgnore
 import io.github.kingg22.ktorgen.http.Cookie
 import io.github.kingg22.ktorgen.http.FormUrlEncoded
+import io.github.kingg22.ktorgen.http.Fragment
 import io.github.kingg22.ktorgen.http.HTTP
 import io.github.kingg22.ktorgen.http.Header
 import io.github.kingg22.ktorgen.http.Multipart
@@ -46,98 +46,20 @@ class FunctionMapper : DeclarationFunctionMapper {
                 DeclarationParameterMapper.DEFAULT.mapToModel(param).also {
                     timer.markStepCompleted("Processed param: ${it.nameString}")
                 }
-            }.also {
-                timer.markStepCompleted("Adding imports of parameters")
-                for (annotation in it.flatMap { a -> a.ktorgenAnnotations }) {
-                    when (annotation) {
-                        ParameterAnnotation.Body,
-                        is ParameterAnnotation.Part,
-                        is ParameterAnnotation.PartMap,
-                        is ParameterAnnotation.Field,
-                        is ParameterAnnotation.FieldMap,
-                        -> {
-                            onAddImport(KTOR_CLIENT_SET_BODY)
-                            onAddImport(KTOR_CONTENT_TYPE)
-                            onAddImport(KTOR_CONTENT_TYPE_ADD)
-                            onAddImport(KTOR_CLIENT_FORM_DATA_CONTENT)
-                            onAddImport(KTOR_CLIENT_MULTI_PART_FORM_DATA_CONTENT)
-                            onAddImport(KTOR_CLIENT_FORM_DATA)
-                            onAddImport(KTOR_PARAMETERS)
-                        }
-
-                        is ParameterAnnotation.Header, ParameterAnnotation.HeaderMap -> onAddImport(
-                            KTOR_CLIENT_HEADERS,
-                        )
-
-                        is ParameterAnnotation.Path -> if (!annotation.encoded) onAddImport(KTOR_ENCODE_URL_PATH)
-                        is ParameterAnnotation.Tag -> onAddImport(KTOR_CLIENT_ATTRIBUTE_KEY)
-                        is ParameterAnnotation.Cookies -> {
-                            onAddImport(KTOR_CLIENT_COOKIE)
-                            onAddImport(KTOR_GMT_DATE)
-                        }
-
-                        is ParameterAnnotation.Query,
-                        is ParameterAnnotation.QueryMap,
-                        is ParameterAnnotation.QueryName,
-                        ParameterAnnotation.Url,
-                        -> {
-                            /* No Op*/
-                        }
-                    }
-                }
             }
+            timer.markStepCompleted("Adding imports of parameters")
+            addImportsForParametersAnnotations(parameters.flatMap { p -> p.ktorgenAnnotations }, onAddImport)
+
             val returnType = TypeData(
                 requireNotNull(declaration.returnType?.resolve()) { KtorGenLogger.FUNCTION_NOT_RETURN_TYPE + name },
             )
             timer.markStepCompleted(
                 "Processed return type: ${returnType.parameterType.declaration.simpleName.asString()}",
             )
-            val httpAnnotations = httpAnnotationResolver(declaration)
-            timer.markStepCompleted("Processed http annotation: $httpAnnotations")
 
-            require(httpAnnotations.isNotEmpty()) { KtorGenLogger.NO_HTTP_ANNOTATION_AT + name }
-
-            require(httpAnnotations.size == 1) {
-                "${KtorGenLogger.ONLY_ONE_HTTP_METHOD_IS_ALLOWED} Found: ${
-                    httpAnnotations.joinToString {
-                        it.httpMethod.value
-                    }
-                } at $name"
-            }
-
-            onAddImport(KTOR_HTTP_METHOD)
-            timer.markStepCompleted("Http method is valid")
-
-            val functionAnnotations = getFunctionAnnotations(declaration, timer).also {
-                timer.markStepCompleted("Processed function annotations, adding imports")
-                for (annotation in it) {
-                    if (annotation is FunctionAnnotation.Headers ||
-                        annotation is FunctionAnnotation.FormUrlEncoded
-                    ) {
-                        onAddImport(KTOR_CLIENT_HEADERS)
-                    }
-
-                    if (annotation is FunctionAnnotation.Cookies) {
-                        onAddImport(KTOR_CLIENT_COOKIE)
-                        onAddImport(KTOR_GMT_DATE)
-                    }
-
-                    if (annotation is FunctionAnnotation.FormUrlEncoded ||
-                        annotation is FunctionAnnotation.Multipart ||
-                        parameters.any { param ->
-                            param.hasAnnotation<ParameterAnnotation.Field>() ||
-                                param.hasAnnotation<ParameterAnnotation.FieldMap>() ||
-                                param.hasAnnotation<ParameterAnnotation.Part>() ||
-                                param.hasAnnotation<ParameterAnnotation.PartMap>()
-                        }
-                    ) {
-                        onAddImport(KTOR_CLIENT_FORM_DATA_CONTENT)
-                        onAddImport(KTOR_CLIENT_MULTI_PART_FORM_DATA_CONTENT)
-                        onAddImport(KTOR_CLIENT_FORM_DATA)
-                        onAddImport(KTOR_PARAMETERS)
-                    }
-                }
-            }
+            val functionAnnotations = getFunctionAnnotations(declaration, timer)
+            timer.markStepCompleted("Processed function annotations, adding imports")
+            addImportsForFunctionAnnotations(functionAnnotations, onAddImport)
 
             val isSuspend = declaration.modifiers.contains(Modifier.SUSPEND)
 
@@ -153,8 +75,9 @@ class FunctionMapper : DeclarationFunctionMapper {
                 isSuspend = isSuspend,
                 isImplemented = declaration.isAbstract.not(),
                 parameterDataList = parameters,
-                httpMethodAnnotation = httpAnnotations.first(),
                 ktorGenAnnotations = functionAnnotations,
+                httpMethodAnnotation =
+                functionAnnotations.filterIsInstance<FunctionAnnotation.HttpMethodAnnotation>().first(),
                 nonKtorGenAnnotations = emptyList(), // TODO
                 modifierSet = modifiers,
                 ksFunctionDeclaration = declaration,
@@ -167,12 +90,112 @@ class FunctionMapper : DeclarationFunctionMapper {
         }
     }
 
+    private fun addImportsForFunctionAnnotations(annotations: List<FunctionAnnotation>, onAddImport: (String) -> Unit) {
+        for (annotation in annotations) {
+            when (annotation) {
+                is FunctionAnnotation.Headers -> {
+                    onAddImport(KTOR_CLIENT_HEADERS)
+                }
+
+                is FunctionAnnotation.Cookies -> {
+                    onAddImport(KTOR_CLIENT_COOKIE)
+                    onAddImport(KTOR_GMT_DATE)
+                }
+
+                is FunctionAnnotation.FormUrlEncoded,
+                is FunctionAnnotation.Multipart,
+                -> {
+                    onAddImport(KTOR_CONTENT_TYPE_ADD)
+                    onAddImport(KTOR_CLIENT_FORM_DATA_CONTENT)
+                    onAddImport(KTOR_CLIENT_MULTI_PART_FORM_DATA_CONTENT)
+                    onAddImport(KTOR_CLIENT_FORM_DATA)
+                    onAddImport(KTOR_PARAMETERS)
+                }
+
+                is FunctionAnnotation.HttpMethodAnnotation -> onAddImport(KTOR_HTTP_METHOD)
+                is FunctionAnnotation.Fragment, FunctionAnnotation.Streaming -> {
+                    /* No Op */
+                }
+            }
+        }
+    }
+
+    private fun addImportsForParametersAnnotations(
+        parameters: List<ParameterAnnotation>,
+        onAddImport: (String) -> Unit,
+    ) {
+        for (annotation in parameters) {
+            when (annotation) {
+                ParameterAnnotation.Body,
+                is ParameterAnnotation.Part,
+                is ParameterAnnotation.PartMap,
+                is ParameterAnnotation.Field,
+                is ParameterAnnotation.FieldMap,
+                -> {
+                    onAddImport(KTOR_CLIENT_SET_BODY)
+                    onAddImport(KTOR_CONTENT_TYPE)
+                    onAddImport(KTOR_CONTENT_TYPE_ADD)
+                    onAddImport(KTOR_CLIENT_FORM_DATA_CONTENT)
+                    onAddImport(KTOR_CLIENT_MULTI_PART_FORM_DATA_CONTENT)
+                    onAddImport(KTOR_CLIENT_FORM_DATA)
+                    onAddImport(KTOR_PARAMETERS)
+                }
+
+                is ParameterAnnotation.Header, ParameterAnnotation.HeaderMap -> onAddImport(
+                    KTOR_CLIENT_HEADERS,
+                )
+
+                is ParameterAnnotation.Path -> if (!annotation.encoded) onAddImport(KTOR_ENCODE_URL_PATH)
+                is ParameterAnnotation.Tag -> onAddImport(KTOR_CLIENT_ATTRIBUTE_KEY)
+                is ParameterAnnotation.Cookies -> {
+                    onAddImport(KTOR_CLIENT_COOKIE)
+                    onAddImport(KTOR_GMT_DATE)
+                }
+
+                is ParameterAnnotation.Query,
+                is ParameterAnnotation.QueryMap,
+                is ParameterAnnotation.QueryName,
+                ParameterAnnotation.Url,
+                -> {
+                    /* No Op*/
+                }
+            }
+        }
+    }
+
     @OptIn(KspExperimental::class)
     private fun getFunctionAnnotations(function: KSFunctionDeclaration, timer: Timer) = buildList {
-        timer.markStepCompleted("Start collect KtorGen annotations, first Ignore")
-        function.getAnnotationsByType(KtorGenIgnore::class).firstOrNull()?.let {
-            add(FunctionAnnotation.Ignore)
-            timer.markStepCompleted("Ignore found")
+        timer.markStepCompleted("Start collect KtorGen annotations, first Http Method")
+
+        val method = httpAnnotationResolver(function)
+        if (method.isEmpty()) {
+            add(FunctionAnnotation.HttpMethodAnnotation("", HttpMethod.Absent))
+            timer.markStepCompleted("Http method not found, adding absent value, need validation!")
+        } else {
+            require(method.size == 1) {
+                "${KtorGenLogger.ONLY_ONE_HTTP_METHOD_IS_ALLOWED} Found: ${method.joinToString {
+                    it.httpMethod.value
+                }} at ${function.simpleName.asString()}"
+            }
+            val http = method.first()
+            add(http)
+            timer.markStepCompleted("Processed http annotation $http")
+        }
+
+        timer.markStepCompleted("Going to get Fragment")
+        function.getAnnotation<Fragment>(
+            manualExtraction = {
+                add(
+                    FunctionAnnotation.Fragment(
+                        it.getArgumentValueByName<String>("value").orEmpty(),
+                        it.getArgumentValueByName("encoded") ?: false,
+                    ),
+                )
+                timer.markStepCompleted("Fragment found")
+            },
+        ) {
+            add(FunctionAnnotation.Fragment(it.value, it.encoded))
+            timer.markStepCompleted("Fragment found")
         }
 
         timer.markStepCompleted("Going to get Multipart")
@@ -216,7 +239,7 @@ class FunctionMapper : DeclarationFunctionMapper {
         timer.markStepCompleted("Finish collection of KtorGen annotations")
     }
 
-    fun httpAnnotationResolver(function: KSFunctionDeclaration): List<FunctionAnnotation.HttpMethodAnnotation> {
+    private fun httpAnnotationResolver(function: KSFunctionDeclaration): List<FunctionAnnotation.HttpMethodAnnotation> {
         @OptIn(KspExperimental::class)
         fun KSFunctionDeclaration.parseHTTPMethod(name: String): FunctionAnnotation.HttpMethodAnnotation? {
             val annotation = this.annotations.firstOrNull { it.shortName.asString() == name }
