9831e9150093ac50bc5ab9e74ed5ec51eab71c17
Rey <89825425+Kingg22@users.noreply.github.com>
2025-08-06 08:04:32 -0500

    feat(ktorgen-compiler): add more valid types for HttpRequestBuilder

--- compiler/src/main/kotlin/io/github/kingg22/ktorgen/model/ParameterData.kt
+++ compiler/src/main/kotlin/io/github/kingg22/ktorgen/model/ParameterData.kt
@@ -9,9 +9,11 @@ class ParameterData(
     val ksValueParameter: KSValueParameter,
     val ktorgenAnnotations: List<ParameterAnnotation> = emptyList(),
     val isHttpRequestBuilderLambda: Boolean = false,
-    val isHttpRequestBuilder: Boolean = typeData.typeName == HttpRequestBuilderTypeName,
+    // see https://api.ktor.io/ktor-client/ktor-client-core/io.ktor.client.request/-http-request-builder/index.html?query=class%20HttpRequestBuilder%20:%20HttpMessageBuilder#-1196439924%2FFunctions%2F-1897681819
+    val isValidTakeFrom: Boolean = typeData.typeName in
+        arrayOf(HttpRequestBuilderTypeName, HttpRequestTypeName, HttpRequestDataTypeName),
 ) {
-    val isVararg: Boolean = ksValueParameter.isVararg
+    val isVararg = ksValueParameter.isVararg
 
     inline fun <reified T : ParameterAnnotation> findAnnotationOrNull() =
         ktorgenAnnotations.filterIsInstance<T>().firstOrNull()
