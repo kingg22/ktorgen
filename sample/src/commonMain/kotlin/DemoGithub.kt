2b2c9e20fb1c62ba23809807c7846d2172e347b2
Rey <89825425+Kingg22@users.noreply.github.com>
2025-08-11 10:55:11 -0500

    test(ktorgen-sample): add experimental annotation on code

--- sample/src/commonMain/kotlin/DemoGithub.kt
+++ sample/src/commonMain/kotlin/DemoGithub.kt
@@ -5,13 +5,17 @@ import io.github.kingg22.ktorgen.http.GET
 import io.github.kingg22.ktorgen.http.Header
 import io.github.kingg22.ktorgen.http.HeaderParam
 import io.github.kingg22.ktorgen.http.Path
+import io.ktor.client.request.HttpRequestBuilder
 
-@KtorGen
+@KtorGen(optInAnnotations = [ExperimentalApi::class])
+@Deprecated("")
+@OptIn(ExperimentalApi::class)
 interface DemoGithub : ApiServiceValid {
     @GET("repos/{owner}/{repo}")
     suspend fun getRepo(@Path owner: String, @Path repo: String, @HeaderParam("Authorization") token: String): String
 
     @Header("Content-Type", "application/json")
     @GET("users")
-    suspend fun getUsers(): List<String>
+    @Deprecated("")
+    suspend fun getUsers(dynamic: HttpRequestBuilder.() -> Unit): List<String>
 }
---
+++ sample/src/commonMain/kotlin/ExperimentalApi.kt
@@ -0,0 +1,4 @@
+package io.github.kingg22.ktorgen.sample
+
+@RequiresOptIn(level = RequiresOptIn.Level.ERROR)
+annotation class ExperimentalApi
