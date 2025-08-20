4cc19f92bd7dd59b71c452ba2bfa2cb6aebf7a86
Rey <89825425+Kingg22@users.noreply.github.com>
2025-08-19 23:49:35 -0500

    test(ktorgen-sample): add advance use of visibility and annotations

--- sample/src/commonMain/kotlin/DemoGithub.kt
+++ sample/src/commonMain/kotlin/DemoGithub.kt
@@ -6,12 +6,15 @@ import io.github.kingg22.ktorgen.http.Header
 import io.github.kingg22.ktorgen.http.HeaderParam
 import io.github.kingg22.ktorgen.http.Path
 import io.ktor.client.request.HttpRequestBuilder
+import kotlin.jvm.JvmSynthetic
 
 @KtorGen(optInAnnotations = [ExperimentalApi::class])
-@Deprecated("")
-@OptIn(ExperimentalApi::class)
-interface DemoGithub : ApiServiceValid {
+@ExperimentalApi
+internal interface DemoGithub : ApiServiceValid {
     @GET("repos/{owner}/{repo}")
+    @Header("Accept", "application/json")
+    @Header(Header.Accept, Header.ContentTypes.Application.Cbor)
+    @JvmSynthetic
     suspend fun getRepo(@Path owner: String, @Path repo: String, @HeaderParam("Authorization") token: String): String
 
     @Header("Content-Type", "application/json")
