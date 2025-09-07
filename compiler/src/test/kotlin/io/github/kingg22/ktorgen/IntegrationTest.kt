e1136b3e32308b8a823af5e8dac91f75ea89da82
Rey <89825425+Kingg22@users.noreply.github.com>
2025-09-06 23:06:38 -0500

    test(ktorgen-compiler): start integration test with room compiler testing

---
+++ compiler/src/test/kotlin/io/github/kingg22/ktorgen/IntegrationTest.kt
@@ -0,0 +1,38 @@
+package io.github.kingg22.ktorgen
+
+import androidx.room.compiler.processing.ExperimentalProcessingApi
+import androidx.room.compiler.processing.util.Source
+import androidx.room.compiler.processing.util.runKspProcessorTest
+import kotlin.test.Test
+
+@OptIn(ExperimentalProcessingApi::class)
+class IntegrationTest {
+    @Test
+    fun getWithBodyThrowWarningsAsError() {
+        val source = Source.kotlin(
+            "foo.bar.MyGetWarnings.kt",
+            """
+                package foo.bar
+
+                import io.github.kingg22.ktorgen.http.*
+
+                data class IssueData(val title: String, val body: String)
+
+                interface MyGetWarnings {
+                    // ⚠️ @GET con @Body
+                    @GET("users/filter")
+                    suspend fun getUsersWithBody(@Body filter: IssueData): List<Any>
+                }
+            """.trimIndent(),
+        )
+
+        runKspProcessorTest(
+            sources = listOf(source),
+            options = mapOf("ktorgen_check_type" to "1"),
+            symbolProcessorProviders = listOf(KtorGenSymbolProcessorProvider()),
+        ) { compilationResultSubject ->
+            compilationResultSubject.hasError()
+            compilationResultSubject.hasErrorCount(1)
+        }
+    }
+}
