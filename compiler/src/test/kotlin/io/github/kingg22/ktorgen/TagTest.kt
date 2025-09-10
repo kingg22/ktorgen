8e4b5e1e16464b32f4479a540437cde0a35d3dc3
Rey <89825425+Kingg22@users.noreply.github.com>
2025-09-09 21:18:06 -0500

    test(ktorgen-compiler): add tag annotation test

---
+++ compiler/src/test/kotlin/io/github/kingg22/ktorgen/TagTest.kt
@@ -0,0 +1,40 @@
+package io.github.kingg22.ktorgen
+
+import androidx.room.compiler.processing.util.Source
+import kotlin.test.Test
+
+class TagTest {
+    @Test
+    fun testTagsAnnotationFoundAddedAsAttributeKey() {
+        val source = Source.kotlin(
+            "Source.kt",
+            """
+                package com.example.api
+
+                import io.github.kingg22.ktorgen.http.GET
+                import io.github.kingg22.ktorgen.http.Tag
+
+                interface TestService {
+                    @GET("posts")
+                    suspend fun test(@Tag myTag1 : String, @Tag("myTag2") someParameter: Int?): String
+                }
+            """.trimIndent(),
+        )
+
+        val expectedHeadersArgumentText = listOf(
+            """attributes.put(AttributeKey("myTag1"), myTag1)""",
+            """attributes.put(AttributeKey("myTag2"), it)""",
+        )
+
+        runKtorGenProcessor(source) { compilationResultSubject ->
+            compilationResultSubject.hasNoWarnings()
+            compilationResultSubject.generatedSourceFileWithPath(
+                """com\example\api\_TestServiceImpl.kt""",
+            ).let { actualSource ->
+                for (expectedLine in expectedHeadersArgumentText) {
+                    actualSource.contains(expectedLine)
+                }
+            }
+        }
+    }
+}
