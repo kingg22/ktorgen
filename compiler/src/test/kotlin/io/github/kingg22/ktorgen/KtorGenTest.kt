01b8ebd91334b9fc8e76c016b3e5a9ab1690b821
Rey <89825425+Kingg22@users.noreply.github.com>
2025-09-10 21:25:51 -0500

    test(ktorgen-compiler): add test of KtorGen propagate annotations and optInAnnotation

--- compiler/src/test/kotlin/io/github/kingg22/ktorgen/KtorGenTest.kt
+++ compiler/src/test/kotlin/io/github/kingg22/ktorgen/KtorGenTest.kt
@@ -47,4 +47,103 @@ class KtorGenTest {
             assertNull(it.findGeneratedSource(TEST_SERVICE_IMPL_PATH))
         }
     }
+
+    @Test
+    fun testOptInAnnotationIsPropagatedInClassGenerated() {
+        val source = Source.kotlin(
+            "Source.kt",
+            """
+                package com.example.api
+
+                import io.github.kingg22.ktorgen.http.GET
+                import io.github.kingg22.ktorgen.http.Header
+
+                import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
+
+                @OptIn(ExperimentalCompilerApi::class)
+                interface TestService {
+                    @Header("x", "y")
+                    @GET("posts")
+                    suspend fun test(): String
+                }
+            """.trimIndent(),
+        )
+
+        val expectedHeadersArgumentText = "@OptIn(ExperimentalCompilerApi::class)"
+
+        runKtorGenProcessor(source) { compilationResultSubject ->
+            compilationResultSubject.hasNoWarnings()
+            val generatedFile = compilationResultSubject.generatedSourceFileWithPath(TEST_SERVICE_IMPL_PATH)
+            generatedFile.contains(expectedHeadersArgumentText)
+            compilationResultSubject.hasErrorCount(0)
+        }
+    }
+
+    @Test
+    fun testRequiredOptInAnnotationIsPropagatedInClassGeneratedWithoutOptInAnnotation() {
+        val source = Source.kotlin(
+            "Source2.kt",
+            """
+                package com.example.api
+
+                import io.github.kingg22.ktorgen.http.GET
+                import io.github.kingg22.ktorgen.http.Header
+
+                import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
+
+                @ExperimentalCompilerApi
+                interface TestService {
+                    @Header("x", "y")
+                    @GET("posts")
+                    suspend fun test(): String
+                }
+            """.trimIndent(),
+        )
+
+        val expectedHeadersArgumentText = "@ExperimentalCompilerApi"
+        val noExpectedHeadersArgumentText = "@OptIn(ExperimentalCompilerApi::class)"
+
+        runKtorGenProcessor(source) { compilationResultSubject ->
+            compilationResultSubject.hasNoWarnings()
+            compilationResultSubject.hasErrorCount(0)
+            val generatedFile = compilationResultSubject.generatedSourceFileWithPath(TEST_SERVICE_IMPL_PATH)
+            generatedFile.contains(expectedHeadersArgumentText)
+            generatedFile.doesNotContain(noExpectedHeadersArgumentText)
+        }
+    }
+
+    @Test
+    fun testRequiredOptInAnnotationIsPropagatedInClassGeneratedWithOptInOption() {
+        val source = Source.kotlin(
+            "Source2.kt",
+            """
+                package com.example.api
+
+                import io.github.kingg22.ktorgen.core.KtorGen
+                import io.github.kingg22.ktorgen.http.GET
+                import io.github.kingg22.ktorgen.http.Header
+
+                import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
+
+                @KtorGen(optInAnnotations = [ExperimentalCompilerApi::class])
+                @ExperimentalCompilerApi
+                interface TestService {
+                    @Header("x", "y")
+                    @GET("posts")
+                    suspend fun test(): String
+                }
+            """.trimIndent(),
+        )
+
+        val expectedHeadersArgumentText = "@OptIn(ExperimentalCompilerApi::class)"
+        val noExpectedHeadersArgumentText = "@ExperimentalCompilerApi"
+
+        runKtorGenProcessor(source) { compilationResultSubject ->
+            compilationResultSubject.hasNoWarnings()
+            compilationResultSubject.hasErrorCount(0)
+            val generatedFile = compilationResultSubject.generatedSourceFileWithPath(TEST_SERVICE_IMPL_PATH)
+            generatedFile.contains(expectedHeadersArgumentText)
+            generatedFile.doesNotContain(noExpectedHeadersArgumentText)
+        }
+    }
 }
