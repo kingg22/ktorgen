22b83e4b8e89ffb8af856f9fca8a562fc1b2f72e
Rey <89825425+Kingg22@users.noreply.github.com>
2025-09-10 19:56:13 -0500

    test(ktorgen-compiler): add unit test to ktorgen no generate option

--- compiler/src/test/kotlin/io/github/kingg22/ktorgen/KtorGenTest.kt
+++ compiler/src/test/kotlin/io/github/kingg22/ktorgen/KtorGenTest.kt
@@ -2,6 +2,7 @@ package io.github.kingg22.ktorgen
 
 import androidx.room.compiler.processing.util.Source
 import kotlin.test.Test
+import kotlin.test.assertNull
 
 /** Test class only for [@KtorGen][io.github.kingg22.ktorgen.core.KtorGen] */
 class KtorGenTest {
@@ -21,6 +22,29 @@ class KtorGenTest {
 
         runKtorGenProcessor(api) { compilationResultSubject ->
             compilationResultSubject.hasErrorCount(0)
+        }
+    }
+
+    @Test
+    fun testNoGenerateOptionSkipValidationAndGeneration() {
+        val source = Source.kotlin(
+            "Source.kt",
+            """
+                package com.example.api
+
+                import io.github.kingg22.ktorgen.core.KtorGen
+
+                @KtorGen(generate=false)
+                interface TestService {
+                    suspend fun hello(): String
+                }
+            """.trimIndent(),
+        )
+
+        runKtorGenProcessor(source) {
+            it.hasNoWarnings()
+            it.hasErrorCount(0)
+            assertNull(it.findGeneratedSource(TEST_SERVICE_IMPL_PATH))
         }
     }
 }
