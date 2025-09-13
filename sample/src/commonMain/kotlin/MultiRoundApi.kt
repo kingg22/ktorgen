c9059b9e062f69b465c2f0ff71a62f7faf669e7f
Rey <89825425+Kingg22@users.noreply.github.com>
2025-09-13 00:15:54 -0500

    feat(sample): try to trigger multi round

---
+++ sample/src/commonMain/kotlin/MultiRoundApi.kt
@@ -0,0 +1,35 @@
+package io.github.kingg22.ktorgen.sample
+
+import io.github.kingg22.ktorgen.core.KtorGen
+import io.github.kingg22.ktorgen.core.KtorGenFunction
+import io.github.kingg22.ktorgen.http.*
+import kotlin.jvm.JvmSynthetic
+
+@KtorGen(
+    classVisibilityModifier = "private",
+    functionAnnotations = [JvmSynthetic::class],
+)
+internal interface MultiRoundApi {
+    @POST
+    @Header("Content-Type", "application/json")
+    @KtorGenFunction(annotations = [JvmSynthetic::class])
+    @JvmSynthetic
+    suspend fun getAlbumDiscography(
+        @Body jsonBody: String,
+        @Query("api_token") apiToken: String = "",
+        @Query("method") method: String = "album.getDiscography",
+        @Query("api_version") apiVersion: Float = 1.0f,
+        @Query input: Int = 3,
+    ): String
+
+    @POST
+    @Header("Content-Type", "application/json")
+    @KtorGenFunction(annotations = [JvmSynthetic::class])
+    suspend fun getAlbumData(
+        @Body jsonBody: String,
+        @Query("api_token") apiToken: String = "",
+        @Query("method") method: String = "album.getData",
+        @Query("api_version") apiVersion: Float = 1.0f,
+        @Query input: Int = 3,
+    ): String
+}
