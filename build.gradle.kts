8dba68d3e9cd99540f8cb69b7ce3ce39cb6663c0
Rey <89825425+kingg22@users.noreply.github.com>
2025-11-04 15:13:43 -0500

    build(project): add sonarqube plugin, move ktlint to root, enable lock dependency
    TODO remove ktlint of subproject gradle files, write lock files, add sonarqube in github action

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,62 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlinxKover) apply false
     alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.ktlint)
     alias(libs.plugins.mavenPublish) apply false
+    alias(libs.plugins.sonarqube)
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "org.sonarqube")
+
+    dependencyLocking {
+        lockAllConfigurations()
+    }
+
+    sonar.properties {
+        // evita incluir coverage de módulos que no tienen tests
+        if (project.name == "annotations") {
+            property("sonar.sources", "src/commonMain")
+            property("sonar.coverage.exclusions", "**/*")
+        }
+
+        if (project.name == "compiler") {
+            property("sonar.sources", "src/main")
+            property("sonar.tests", "src/test")
+            property(
+                "sonar.coverage.jacoco.xmlReportPaths",
+                project.layout.buildDirectory.file("reports/kover/report.xml").get().asFile.absolutePath,
+            )
+        }
+
+        // ignora completamente estos módulos
+        if (project.name == "example" || project.name == "sample") {
+            property("sonar.skipProject", "true")
+        }
+    }
+}
+
+sonar.properties {
+    property("sonar.projectKey", "kingg22_ktorgen")
+    property("sonar.organization", "kingg22")
+    property("sonar.projectName", "ktorgen")
+    property("sonar.projectVersion", libs.versions.ktorgen.version.get())
+    property("sonar.gradle.scanAll", false) // Conflict with submodules config
+    property("sonar.scanner.skipJreProvisioning", true)
+    property("sonar.scanner.javaExePath", "${System.getProperty("java.home")}/bin/java")
+}
+
+tasks.sonar {
+    onlyIf {
+        System.getProperty("java.home") != null && System.getenv("SONAR_TOKEN") != null
+    }
+}
+
+ktlint {
+    version.set(libs.versions.ktlint.pinterest.get())
+}
+
+dependencyLocking {
+    lockAllConfigurations()
 }
