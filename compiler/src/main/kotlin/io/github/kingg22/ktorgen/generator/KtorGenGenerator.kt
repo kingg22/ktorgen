617a4cd0f6e74cbd418cddbaf6dcad6b3184bfe1
Rey <89825425+kingg22@users.noreply.github.com>
2025-11-05 02:14:52 -0500

    perf(compiler, generator): revert unique default instance

--- compiler/src/main/kotlin/io/github/kingg22/ktorgen/generator/KtorGenGenerator.kt
+++ compiler/src/main/kotlin/io/github/kingg22/ktorgen/generator/KtorGenGenerator.kt
@@ -1,23 +1,15 @@
 package io.github.kingg22.ktorgen.generator
 
-import com.google.devtools.ksp.processing.CodeGenerator
 import com.squareup.kotlinpoet.FileSpec
-import com.squareup.kotlinpoet.ksp.writeTo
 import io.github.kingg22.ktorgen.DiagnosticSender
 import io.github.kingg22.ktorgen.model.ClassData
 
 fun interface KtorGenGenerator {
-    fun generate(classData: ClassData, timer: DiagnosticSender): List<FileSpec>
+    context(timer: DiagnosticSender)
+    fun generate(classData: ClassData): List<FileSpec>
 
     companion object {
-        /** Generate the Impl class using [KotlinpoetGenerator] of ksp */
         @JvmStatic
-        fun generateKsp(classData: ClassData, codeGenerator: CodeGenerator, timer: DiagnosticSender) {
-            DEFAULT.generate(classData, timer).forEach { it.writeTo(codeGenerator, false) }
-        }
-
-        @JvmStatic
-        val DEFAULT: KtorGenGenerator
-            get() = KotlinpoetGenerator()
+        val DEFAULT: KtorGenGenerator by lazy { KotlinpoetGenerator() }
     }
 }
