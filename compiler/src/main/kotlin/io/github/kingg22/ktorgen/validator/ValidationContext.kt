87270b17fe5cc3446a5200f2f352ed469412aa47
Rey <89825425+kingg22@users.noreply.github.com>
2025-11-07 18:54:12 -0500

    refactor(compiler, validator): create a snapshot of class data and use the sequence API
    This is intent to improve the validation pipeline, but needs to split in warnings/error or per phase layer, IDK sure what strategy implements

--- compiler/src/main/kotlin/io/github/kingg22/ktorgen/validator/ValidationContext.kt
+++ compiler/src/main/kotlin/io/github/kingg22/ktorgen/validator/ValidationContext.kt
@@ -1,9 +1,39 @@
 package io.github.kingg22.ktorgen.validator
 
+import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSNode
+import com.squareup.kotlinpoet.KModifier
 import io.github.kingg22.ktorgen.model.ClassData
-import io.github.kingg22.ktorgen.model.FunctionData
+import io.github.kingg22.ktorgen.model.ClassGenerationOptions
 
-class ValidationContext(internal val classData: ClassData) {
+class ValidationContext(classData: ClassData) {
     // shortcuts
-    internal val functions: List<FunctionData> = classData.functions
+    val classData = SnapshotClassData(classData)
+    val expectFunctions = classData.expectFunctions
+    val functions = classData.functions.asSequence()
+
+    class SnapshotClassData private constructor(
+        val packageNameString: String,
+        val interfaceName: String,
+        val modifierSet: Set<KModifier>,
+        val companionObjectDeclaration: KSClassDeclaration?,
+        val isKtorGenOnClass: Boolean,
+        val isKtorGenOnCompanionObject: Boolean,
+        options: ClassGenerationOptions,
+        ksClass: KSNode,
+    ) : ClassGenerationOptions(options),
+        KSNode by ksClass {
+        constructor(
+            classData: ClassData,
+        ) : this(
+            packageNameString = classData.packageNameString,
+            interfaceName = classData.interfaceName,
+            modifierSet = classData.modifierSet,
+            companionObjectDeclaration = classData.companionObjectDeclaration,
+            isKtorGenOnClass = classData.isKtorGenAnnotationDeclaredOnClass,
+            isKtorGenOnCompanionObject = classData.isKtorGenAnnotationDeclaredOnCompanionClass,
+            options = classData,
+            ksClass = classData.ksClassDeclaration,
+        )
+    }
 }
