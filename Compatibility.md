b960a3d5b250557c83c99257afbbd7db3b0d5fad
Rey <89825425+kingg22@users.noreply.github.com>
2025-12-30 22:42:16 -0500

    build(ktorgen): add compatibility script and update compatibility matrix
    TODO add a table in documentation with results

---
+++ Compatibility.md
@@ -0,0 +1,47 @@
+# Compatibility configuration
+
+Versions released since 2024 until 30th December 2025
+
+Filtered since the minimum required version of Kotlin of KtorGen: 2.2.20 according to
+[androidx](https://github.com/androidx/androidx/blob/androidx-main/gradle/libs.versions.toml#L55)
+
+Extracted from [GitHub Releases](https://github.com/ktorio/ktor/releases) with GH CLI without pre-releases:
+
+```json
+ktor:
+[18]{publishedAt,tagName}:
+    "2025-11-27T12:13:53Z",3.3.3
+    "2025-11-05T14:08:06Z",3.3.2
+    "2025-10-10T06:19:56Z",3.3.1
+    "2025-09-12T11:59:39Z",3.3.0
+    "2025-07-29T14:18:09Z",3.2.3
+    "2025-07-14T11:45:21Z",3.2.2
+    "2025-07-04T08:57:57Z",3.2.1
+    "2025-06-13T12:19:28Z",3.2.0
+    "2025-05-06T11:52:28Z",3.1.3
+    "2025-03-28T09:27:52Z",3.1.2
+    "2025-02-25T11:49:42Z",3.1.1
+    "2025-02-12T11:44:49Z",3.1.0
+    "2024-12-19T14:25:41Z",3.0.3
+    "2024-12-04T09:48:12Z",3.0.2
+    "2024-11-21T09:34:48Z",2.3.13
+    "2024-10-30T08:46:03Z",3.0.1
+    "2024-10-10T09:46:01Z",3.0.0
+    "2024-06-21T08:04:56Z",2.3.12
+kotlin:
+[3]{publishedAt,tagName}:
+    "2025-12-16T13:29:31Z",v2.3.0
+    "2025-10-23T10:35:10Z",v2.2.21
+    "2025-09-10T08:33:26Z",v2.2.20
+ksp:
+[9]{publishedAt,tagName}:
+    "2025-12-16T20:35:23Z",2.3.4
+    "2025-11-20T20:58:40Z",2.3.3
+    "2025-11-06T20:22:33Z",2.3.2
+    "2025-11-04T17:29:37Z",2.3.1
+    "2025-10-28T20:11:15Z",2.2.21-2.0.4
+    "2025-10-22T16:26:19Z",2.3.0
+    "2025-10-07T21:52:47Z",2.2.20-2.0.4
+    "2025-09-11T21:48:29Z",2.2.20-2.0.3
+    "2025-09-10T21:04:57Z",2.2.20-2.0.2
+```
---
+++ compatibility_test.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+# run_case KOTLIN KSP KTOR
+# ------------------------
+# Test all samples compiles with the local ktorgen version
+#
+# Parameters:
+#   KOTLIN   The kotlin version to compile
+#   KSP      The KSP version to use as plugin, needs to match the kotlin version when target KSP < 2.3.0
+#   KTOR     The Ktor Client version to compile
+run_case() {
+    # a case for test
+    echo "------------------------------------------------"
+    echo "Building samples with Kotlin '$1', KSP '$2', Ktor '$3'"
+    if ktorgen_samples_local=true \
+        ktorgen_samples_kotlin_version=$1 \
+        ktorgen_samples_ksp_version=$2 \
+        ktorgen_samples_ktor_version=$3 \
+        ./gradlew clean kotlinUpgradeYarnLock kotlinWasmUpgradeYarnLock build --stacktrace --warning-mode summary
+    then
+      echo "Success compilation."
+    else
+      echo "Failure build" >&2
+      exit 1
+    fi
+}
+
+echo "Compatibility Matrix Testing for KtorGen"
+
+cd samples &> /dev/null && pwd
+
+# run_case 2.3.0 2.3.4 3.3.3 # Success
+# run_case 2.2.21 2.2.21-2.0.4 3.2.3 # Success
+# run_case 2.2.20 2.2.20-2.0.4 3.2.0 # Success
+# run_case 2.2.20 2.2.20-2.0.4 3.1.3 # Success
+
+# Failed. Kotlin libraries (ktorgen-annotations -> kotlin-stdlib) was compiled with kotlin = "2.2.20"
+# run_case 2.2.0 2.2.0-2.0.2 3.2.0
+# run_case 2.1.20 2.1.20-1.0.32 3.0.0
+# run_case 2.0.21 2.0.21-1.0.28 2.3.13
+# run_case 1.9.24 1.9.24-1.0.20 2.3.0
+
+echo "----------------------------------------------"
+echo "Cleaning samples project with default versions"
+run_case '' '' ''
+
+echo "---------------------------------------------------"
+echo "Finish testing of KtorGen"
