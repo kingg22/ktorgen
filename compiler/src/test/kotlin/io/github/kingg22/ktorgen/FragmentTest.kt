4fb78c2ca585da37bb9bf45abaa1bbdb1f77ad63
Rey <89825425+kingg22@users.noreply.github.com>
2025-12-30 22:20:31 -0500

    test(compiler): add tests for fragment parameters and errors

--- compiler/src/test/kotlin/io/github/kingg22/ktorgen/FragmentTest.kt
+++ compiler/src/test/kotlin/io/github/kingg22/ktorgen/FragmentTest.kt
@@ -58,4 +58,191 @@ class FragmentTest {
             generatedFile.contains(expected)
         }
     }
+
+    @Test
+    fun testFragmentEmpty_throwsError() {
+        val source = Source.kotlin(
+            "Source.kt",
+            """
+                package com.example.api
+
+                import io.github.kingg22.ktorgen.http.GET
+                import io.github.kingg22.ktorgen.http.Fragment
+
+                interface TestService {
+                    @GET("posts")
+                    @Fragment(encoded = true)
+                    suspend fun test(): String
+                }
+            """.trimIndent(),
+        )
+
+        runKtorGenProcessor(source) { compilationResultSubject ->
+            compilationResultSubject.hasNoWarnings()
+            compilationResultSubject.hasErrorCount(1)
+            compilationResultSubject.hasErrorContaining(KtorGenLogger.URL_FRAGMENT_IN_FUNCTION_IS_BLANK)
+        }
+    }
+
+    // -- on parameter target --
+    @Test
+    fun testFragmentParam_generatesFragmentAssignmentParam() {
+        val source = Source.kotlin(
+            "Source.kt",
+            """
+                package com.example.api
+
+                import io.github.kingg22.ktorgen.http.GET
+                import io.github.kingg22.ktorgen.http.Fragment
+
+                interface TestService {
+                    @GET("posts")
+                    suspend fun test(@Fragment section: String): String
+                }
+            """.trimIndent(),
+        )
+
+        val expected = $$"this.fragment = \"$section\""
+
+        runKtorGenProcessor(source) { compilationResultSubject ->
+            compilationResultSubject.hasNoWarnings()
+            compilationResultSubject.hasErrorCount(0)
+            val generatedFile = compilationResultSubject.generatedSourceFileWithPath(TEST_SERVICE_IMPL_PATH)
+            generatedFile.contains(expected)
+        }
+    }
+
+    @Test
+    fun testFragmentNullableParam_generatesFragmentAssignmentParamSafety() {
+        val source = Source.kotlin(
+            "Source.kt",
+            """
+                package com.example.api
+
+                import io.github.kingg22.ktorgen.http.GET
+                import io.github.kingg22.ktorgen.http.Fragment
+
+                interface TestService {
+                    @GET("posts")
+                    suspend fun test(@Fragment section: String?): String
+                }
+            """.trimIndent(),
+        )
+
+        val expected = listOf("section?.let", $$"this.fragment = \"$it\"")
+
+        runKtorGenProcessor(source) { compilationResultSubject ->
+            compilationResultSubject.hasNoWarnings()
+            compilationResultSubject.hasErrorCount(0)
+            val generatedFile = compilationResultSubject.generatedSourceFileWithPath(TEST_SERVICE_IMPL_PATH)
+            for (expected in expected) {
+                generatedFile.contains(expected)
+            }
+        }
+    }
+
+    @Test
+    fun testEncodedFragmentParam_generatesFragmentAssignmentParam() {
+        val source = Source.kotlin(
+            "Source.kt",
+            """
+                package com.example.api
+
+                import io.github.kingg22.ktorgen.http.GET
+                import io.github.kingg22.ktorgen.http.Fragment
+
+                interface TestService {
+                    @GET("posts")
+                    suspend fun test(@Fragment(encoded = true) section: String): String
+                }
+            """.trimIndent(),
+        )
+
+        val expected = $$"this.encodedFragment = \"$section\""
+
+        runKtorGenProcessor(source) { compilationResultSubject ->
+            compilationResultSubject.hasNoWarnings()
+            compilationResultSubject.hasErrorCount(0)
+            val generatedFile = compilationResultSubject.generatedSourceFileWithPath(TEST_SERVICE_IMPL_PATH)
+            generatedFile.contains(expected)
+        }
+    }
+
+    @Test
+    fun testEncodedFragmentNullableParam_generatesFragmentAssignmentParamSafety() {
+        val source = Source.kotlin(
+            "Source.kt",
+            """
+                package com.example.api
+
+                import io.github.kingg22.ktorgen.http.GET
+                import io.github.kingg22.ktorgen.http.Fragment
+
+                interface TestService {
+                    @GET("posts")
+                    suspend fun test(@Fragment(encoded = true) section: String?): String
+                }
+            """.trimIndent(),
+        )
+
+        val expected = listOf("section?.let", $$"this.encodedFragment = \"$it\"")
+
+        runKtorGenProcessor(source) { compilationResultSubject ->
+            compilationResultSubject.hasNoWarnings()
+            compilationResultSubject.hasErrorCount(0)
+            val generatedFile = compilationResultSubject.generatedSourceFileWithPath(TEST_SERVICE_IMPL_PATH)
+            for (expected in expected) {
+                generatedFile.contains(expected)
+            }
+        }
+    }
+
+    @Test
+    fun testMultipleFragmentParam_throwsError() {
+        val source = Source.kotlin(
+            "Source.kt",
+            """
+                package com.example.api
+
+                import io.github.kingg22.ktorgen.http.GET
+                import io.github.kingg22.ktorgen.http.Fragment
+
+                interface TestService {
+                    @GET("posts")
+                    suspend fun test(@Fragment section: String, @Fragment subSection: String): String
+                }
+            """.trimIndent(),
+        )
+
+        runKtorGenProcessor(source) { compilationResultSubject ->
+            compilationResultSubject.hasNoWarnings()
+            compilationResultSubject.hasErrorCount(1)
+            compilationResultSubject.hasErrorContaining(KtorGenLogger.MULTIPLE_URL_FRAGMENT)
+        }
+    }
+
+    @Test
+    fun testMultipleFragment_throwsError() {
+        val source = Source.kotlin(
+            "Source.kt",
+            """
+                package com.example.api
+
+                import io.github.kingg22.ktorgen.http.GET
+                import io.github.kingg22.ktorgen.http.Fragment
+
+                interface TestService {
+                    @GET("posts")
+                    @Fragment("#L")
+                    suspend fun test(@Fragment section: String): String
+                }
+            """.trimIndent(),
+        )
+
+        runKtorGenProcessor(source) { compilationResultSubject ->
+            compilationResultSubject.hasNoWarnings()
+            compilationResultSubject.hasErrorCount(1)
+            compilationResultSubject.hasErrorContaining(KtorGenLogger.MULTIPLE_URL_FRAGMENT)
+        }
+    }
 }
