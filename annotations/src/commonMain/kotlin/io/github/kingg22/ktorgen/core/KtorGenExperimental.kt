6be08b5c19102e5dfd196f8b7ab77e9da3de99a2
Rey <89825425+kingg22@users.noreply.github.com>
2026-02-19 16:23:42 -0500

    perf(annotations): restrict `@KtorGenExperimental` to my annotations package
    Breaking change, but it's expected no body use my experimental annotation in your code

--- annotations/src/commonMain/kotlin/io/github/kingg22/ktorgen/core/KtorGenExperimental.kt
+++ annotations/src/commonMain/kotlin/io/github/kingg22/ktorgen/core/KtorGenExperimental.kt
@@ -1,7 +1,21 @@
 package io.github.kingg22.ktorgen.core
 
-/** Experimental API, the compiler can omit the property or annotation, or generated code has errors. */
+/**
+ * Experimental API, the processor can omit the annotation, or generated code has errors.
+ *
+ * To enable the experimental API, add the following to your `build.gradle(.kts)`:
+ * ```kotlin
+ * ksp {
+ *   arg("ktorgen.experimental", "true")
+ * }
+ * ```
+ * Otherwise, the processor will throw an error and/or skip the experimental API.
+ *
+ * This annotation is restricted to this package, the generated code is going to omit this annotation (or opt in of this).
+ * Please don't use this annotation in your code.
+ */
 @RequiresOptIn("KtorGen experimental API, the generated code may be fail or not generated", RequiresOptIn.Level.ERROR)
+@Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.BINARY)
 @MustBeDocumented
 annotation class KtorGenExperimental
