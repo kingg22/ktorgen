b050f8b9be2a4e867554ac54444ede9623fb2bc1
Rey <89825425+kingg22@users.noreply.github.com>
2026-02-19 18:58:01 -0500

    refactor(compiler): remove unnecessary snapshot class data in ValidationContext

--- compiler/src/main/kotlin/io/github/kingg22/ktorgen/validator/ValidationContext.kt
+++ compiler/src/main/kotlin/io/github/kingg22/ktorgen/validator/ValidationContext.kt
@@ -1,39 +1,9 @@
 package io.github.kingg22.ktorgen.validator
 
-import com.google.devtools.ksp.symbol.KSClassDeclaration
-import com.google.devtools.ksp.symbol.KSNode
-import com.squareup.kotlinpoet.KModifier
 import io.github.kingg22.ktorgen.model.ClassData
-import io.github.kingg22.ktorgen.model.ClassGenerationOptions
 
-class ValidationContext(classData: ClassData) {
+class ValidationContext(val classData: ClassData) {
     // shortcuts
-    val classData = SnapshotClassData(classData)
-    val expectFunctions = classData.expectFunctions
-    val functions = classData.functions
-
-    class SnapshotClassData private constructor(
-        val packageNameString: String,
-        val interfaceName: String,
-        val modifierSet: Set<KModifier>,
-        val companionObjectDeclaration: KSClassDeclaration?,
-        val isKtorGenOnClass: Boolean,
-        val isKtorGenOnCompanionObject: Boolean,
-        options: ClassGenerationOptions,
-        ksClass: KSNode,
-    ) : ClassGenerationOptions(options),
-        KSNode by ksClass {
-        constructor(
-            classData: ClassData,
-        ) : this(
-            packageNameString = classData.packageNameString,
-            interfaceName = classData.interfaceName,
-            modifierSet = classData.modifierSet,
-            companionObjectDeclaration = classData.companionObjectDeclaration,
-            isKtorGenOnClass = classData.isKtorGenAnnotationDeclaredOnClass,
-            isKtorGenOnCompanionObject = classData.isKtorGenAnnotationDeclaredOnCompanionClass,
-            options = classData,
-            ksClass = classData.ksClassDeclaration,
-        )
-    }
+    val expectFunctions inline get() = classData.expectFunctions
+    val functions inline get() = classData.functions
 }
